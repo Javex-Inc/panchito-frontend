45c6f360a2cba5e6b1fe0b9e5cab64ca596bac27
Bruno César <bcgmeireles@gmail.com>
2025-01-08 14:32:18 -0300

    feat: implements dynamic source to allow base 64 image

--- src/pages/Catalog/index.jsx
+++ src/pages/Catalog/index.jsx
@@ -1,16 +1,31 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './style.css'
 import Slider from '../../components/Slider';
+import axios from 'axios';
 
 function Catalog() {
+    const [products, setProducts] = useState([]);
     const [selectedIndex, setSelectedIndex] = useState(0);
     const options = ['Categoria', 'Coleção', 'Oferta'];
-
+    
     const examples = [
         'Categoria1', 'Categoria2', 'Categoria3', 'Categoria4', 'Categoria5', 'Categoria6',
         'Coleção1', 'Coleção2', 'Coleção3', 'Coleção4', 'Coleção5', 'Coleção6',
         'Oferta1', 'Oferta2', 'Oferta3', 'Oferta4', 'Oferta5', 'Oferta6',
     ];
+
+    const fetchProducts = async () => {
+        try {
+            const response = await axios.get('http://localhost:8080/products');
+            setProducts(response.data);
+        } catch (error) {
+            console.log("failed to request: ", error);
+        }
+    }
+    
+    useEffect(() => {
+        fetchProducts()
+    }, []);
 
     return (
         <div className='catalogContainer'>
@@ -29,6 +44,17 @@ function Catalog() {
                         <li key={index}>
                             {opt}
                         </li>
+                    ))
+                }
+            </ul>
+
+            <ul>
+                {
+                    products.map((product, key) => (
+                        <>
+                            <p>{product.name}</p>
+                            <img key={key} src={`data:image/jpeg;base64,${product.image}`} alt="" />
+                        </>
                     ))
                 }
             </ul>
